cc995632e91c25fd1169f7ae540d144b97f75a7f
zhangfanhang <543629463@qq.com>
2022-09-25 20:40:43 +0800

    服务器自动部署测试

--- deploy.js
+++ deploy.js
@@ -5,20 +5,22 @@
 const scpClient = require('scp2')
 const ora = require('ora')
 const server = {
-  host: '103.212.34.78', //服务器IP
+  host: '47.242.159.58', //服务器IP
   port: 22, //服务器端口
   username: 'root', //服务器ssh登录用户名
-  password: 'HKzfh4670.', //服务器ssh登录密码
-  path: '/www/wwwroot/notes.zfhblog.top', //服务器web目录
+  password: 'BTzfh4670', //服务器ssh登录密码
+  path: '/www/wwwroot/zfhblog.top', //服务器web目录
 }
 const loading = ora('正在部署至 ' + server.host)
 loading.start()
+const start = Date.now()
 scpClient.scp('dist/', server, err => {
   loading.stop()
   if (err) {
     console.log('部署失败')
     throw err
   } else {
-    console.log('部署成功')
+    const end = Date.now()
+    console.log(`部署成功,用时${(end - start) / 60 / 1000}分钟`)
   }
 })
