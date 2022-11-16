af1533e8059b94620bebc61ccbe708f39892e77f
zhangfanhang <543629463@qq.com>
2022-11-16 23:52:51 +0800

    20221116

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -30,7 +30,25 @@ export default hopeTheme({
     roundAvatar: true,
     avatar:
       'https://zfh-nanjing-bucket.oss-cn-nanjing.aliyuncs.com/blog-images/avatar.jpg',
-    description: '前端菜鸡,目前在南京工作。',
+    description: `前端菜鸡,目前在南京工作。<br>
+    💻开发工具:<br>
+    <img src='https://img.shields.io/badge/-MacBookPro2020-000?style=flat-square&logo=Apple&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-WebStorm-000?style=flat-square&logo=WebStorm&logoColor=white'/>
+    <br>
+    👌正在使用:<br>
+    <img src='https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-TypeScript-3178C6?style=flat-square&logo=typescript&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-Vue-4FC08D?style=flat-square&logo=Vue.js&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-React-61DAFB?style=flat-square&logo=React&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-Markdown-000?style=flat-square&logo=Markdown&logoColor=white'/>
+    <br>
+    💪正在学习:<br>
+    <img src='https://img.shields.io/badge/-Nest-E0234E?style=flat-square&logo=NestJS&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-MySQL-4479A1?style=flat-square&logo=MySQL&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-Docker-2496ED?style=flat-square&logo=Docker&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=Git&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-Java-yellow?style=flat-square'/>
+    `,
     medias: {
       Email: 'mailto:zhangfanhang@znv.com',
       Github: 'https://github.com/zhangfanhang',
