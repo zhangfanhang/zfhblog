81b44e233e524c8f8683100704e12f66142d8096
zhangfanhang <543629463@qq.com>
2022-11-18 00:48:09 +0800

    20221118

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -54,13 +54,16 @@ export default hopeTheme({
     <img src='https://img.shields.io/badge/-Koa-33333D?style=flat-square&logo=Koa&logoColor=white'/>
     <img src='https://img.shields.io/badge/-Node.js-339933?style=flat-square&logo=Node.js&logoColor=white'/>
     <img src='https://img.shields.io/badge/-Redux-764ABC?style=flat-square&logo=Redux&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-iTerm2-000?style=flat-square&logo=iTerm2&logoColor=white'/>
     <img src='https://img.shields.io/badge/-React Router-CA4245?style=flat-square&logo=React Router&logoColor=white'/>
     <img src='https://img.shields.io/badge/-ElementUI-3F9EFF?style=flat-square&logo=/e/&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-MongoDB-47A248?style=flat-square&logo=MongoDB&logoColor=white'/>
     <img src='https://img.shields.io/badge/-Ant Design-0170FE?style=flat-square&logo=Ant Design&logoColor=white'/>
     <br>
     <hr>
     💪正在学习:<br>
-    <img src='https://img.shields.io/badge/-Nest-E0234E?style=flat-square&logo=NestJS&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-Three.js-000?style=flat-square&logo=Three.js&logoColor=white'/>
+    <img src='https://img.shields.io/badge/-NestJS-E0234E?style=flat-square&logo=NestJS&logoColor=white'/>
     <img src='https://img.shields.io/badge/-MySQL-4479A1?style=flat-square&logo=MySQL&logoColor=white'/>
     <img src='https://img.shields.io/badge/-Docker-2496ED?style=flat-square&logo=Docker&logoColor=white'/>
     <img src='https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=Git&logoColor=white'/>
